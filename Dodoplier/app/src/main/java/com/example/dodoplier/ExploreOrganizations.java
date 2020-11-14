161460aa2861b47d72848a641b6dae8ad8441ed1
Mulan Zhao <mulanzhao@Mulans-MacBook-Pro.local>
2020-11-14 00:59:15 -0800

    added bottom nav bars on everything and added fonts

--- Dodoplier/app/src/main/java/com/example/dodoplier/ExploreOrganizations.java
+++ Dodoplier/app/src/main/java/com/example/dodoplier/ExploreOrganizations.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class explore_organizations extends AppCompatActivity {
+public class ExploreOrganizations extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
