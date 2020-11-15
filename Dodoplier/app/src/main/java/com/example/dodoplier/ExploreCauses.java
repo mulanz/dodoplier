86ad6338d5c9fd1f68a0cae1a1a2433fc4ef662b
Mulan Zhao <mulanzhao@Mulans-MacBook-Pro.local>
2020-11-15 01:02:03 -0800

    figured out the api stuff

--- Dodoplier/app/src/main/java/com/example/dodoplier/ExploreCauses.java
+++ Dodoplier/app/src/main/java/com/example/dodoplier/ExploreCauses.java
@@ -4,11 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
-public class ExploreCauses extends AppCompatActivity {
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.function.LongFunction;
+
+public class ExploreCauses extends AppCompatActivity {
+    String catName;
+    String app_id = "5e3ddec9";
+    String api_key = "5032a030035589971487a4a8b4cd5822";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,13 +33,109 @@ public class ExploreCauses extends AppCompatActivity {
 
     public void causeClicked(View view) {
         int categoryID;
-        switch (view.getId()) {
+        int viewID = view.getId();
+        switch (viewID) {
             case R.id.animals:
                 categoryID = 1;
-            case R.id.arts: 
+                catName = "animals";
+                break;
+            case R.id.arts:
+                categoryID = 2;
+                catName = "arts + culture";
+                break;
+            case R.id.community:
+                categoryID = 3;
+                catName = "community";
+                break;
+            case R.id.education:
+                categoryID = 4;
+                catName = "education";
+                break;
+            case R.id.environment:
+                categoryID = 5;
+                catName = "environment";
+                break;
+            case R.id.health:
+                categoryID = 6;
+                catName = "health";
+                break;
+            case R.id.human:
+                categoryID = 7;
+                catName = "human";
+                break;
+            case R.id.human_services:
+                categoryID = 8;
+                catName = "human services";
+                break;
+            default:
+                categoryID = 1;
+        }
+        Log.d("category", Integer.toString(categoryID));
+        Log.d("category name", catName);
+        String url = createURL(categoryID);
+        sendGetRequest(url);
+    }
+
+    private void sendGetRequest(String url) {
+        RequestQueue queue = Volley.newRequestQueue(ExploreCauses.this);
+        final StringRequest request = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    parseResponse(response);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+        queue.add(request);
+    }
+
+    private void parseResponse(String response) throws JSONException {
+        int[] random = new int[5];
+        String[] orgNames = new String[5];
+        String[] orgEIDs = new String[5];
+
+        JSONArray jsonResponse = new JSONArray(response);
+        int numCharities = jsonResponse.length();
+        Log.d("num charities", Integer.toString(numCharities));
+
+        for (int i = 0; i < 5; i ++) {
+            random[i] = (int)(Math.random()*numCharities);
         }
 
+        for (int i = 0; i < random.length; i ++) {
+            JSONObject charityObject = jsonResponse.getJSONObject(random[i]);
+            if (charityObject.has("charityName")) {
+                orgNames[i] = charityObject.getString("charityName");
+            }
+
+            if (charityObject.has("ein")) {
+                orgEIDs[i] = charityObject.getString("ein");
+            }
+        }
+
+        Log.d("first org", orgNames[0]);
+        Log.d("first EID", orgEIDs[0]);
+
         Intent goToOrgsExplorer = new Intent(this, ExploreOrganizations.class);
+        goToOrgsExplorer.putExtra("orgNames", orgNames);
+        goToOrgsExplorer.putExtra("orgEIDs", orgEIDs);
+        goToOrgsExplorer.putExtra("categoryName", catName);
         startActivity(goToOrgsExplorer);
+    }
+
+    private String createURL(int categoryID) {
+        System.out.print("entered");
+        String base = "https://api.data.charitynavigator.org/v2/Organizations?";
+        String id = "app_id=" + app_id;
+        String key = "app_key=" + api_key;
+        String url = base + id + "&" + key + "&" + "rated=true" + "&" + "categoryID=" + categoryID + "&" + "minRating=4";
+        return url;
     }
 }
--- Dodoplier/app/src/main/java/com/example/dodoplier/ExploreOrganizations.java
+++ Dodoplier/app/src/main/java/com/example/dodoplier/ExploreOrganizations.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class ExploreOrganizations extends AppCompatActivity {
@@ -16,6 +17,28 @@ public class ExploreOrganizations extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_explore_organizations);
+
+        String[] orgNames = getIntent().getStringArrayExtra("orgNames");
+        String[] orgEIDs = getIntent().getStringArrayExtra("orgEIDs");
+
+        TextView displayLine = (TextView) findViewById(R.id.orgs_title);
+        displayLine.setText("Sending love to " + getIntent().getStringExtra("categoryName") + ".");
+
+        Button org1 = (Button) findViewById(R.id.org1);
+        org1.setText(orgNames[0]);
+
+        Button org2 = (Button) findViewById(R.id.org2);
+        org2.setText(orgNames[1]);
+
+        Button org3 = (Button) findViewById(R.id.org3);
+        org3.setText(orgNames[2]);
+
+        Button org4 = (Button) findViewById(R.id.org4);
+        org4.setText(orgNames[3]);
+
+        Button org5 = (Button) findViewById(R.id.org5);
+        org5.setText(orgNames[4]);
+
     }
 
     public void orgClicked(View view) {
