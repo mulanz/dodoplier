71dae2f0ee59fa1429e1f6a5d2c843472f09f98b
milanzq309 <milanzq309@gmail.com>
2020-11-15 00:39:50 -0800

    finished profile page

--- Dodoplier/app/src/main/java/com/example/dodoplier/Profile.java
+++ Dodoplier/app/src/main/java/com/example/dodoplier/Profile.java
@@ -2,7 +2,10 @@ package com.example.dodoplier;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class Profile extends AppCompatActivity {
 
@@ -11,4 +14,22 @@ public class Profile extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
     }
+
+    boolean opened = false;
+    public void impactClicked(View view){
+        if (!opened) {
+            TextView explanation = (TextView) findViewById(R.id.explanation);
+            explanation.setVisibility(View.VISIBLE);
+            TextView impact = (TextView) findViewById(R.id.impact);
+            impact.setText("@string/underlined2");
+            opened = true;
+        } else{
+            TextView explanation = (TextView) findViewById(R.id.explanation);
+            explanation.setVisibility(View.GONE);
+            TextView impact = (TextView) findViewById(R.id.impact);
+            impact.setText("@string/underlined");
+            opened = false;
+        }
+    }
+
 }
